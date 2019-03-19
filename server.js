71d693f6fbcbeb343a4aa7da04e0134fa4b171d8
helenpidoulas <helen.pidoulas@gmail.com>
2019-03-19 22:27:29 +1100

    progress commit

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
 const express = require('express');
+//const apiKey = '8a1a3cc956c204ebbbd40834bdd9dc38';
+const apiKey = '8a1a3cc956c204ebbbd40834bdd9dc38';
+const city = 'Sydney';
+const weather = 'Todays weather';
+
+
 const bodyParser = require('body-parser');
 const app = express()
 
@@ -10,9 +16,11 @@ app.get('/', function (req, res) {
   res.render('index');
 })
 app.post('/', function (req, res) {
-  console.log(req.body.city);
   res.render('index');
+  console.log(req.body.city);
 })
+
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
