7b87a5a71c585b89fe74e737dd030066f72872ec
helenpidoulas <helen.pidoulas@gmail.com>
2019-03-19 22:17:02 +1100

    progress commit

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
-// boilerplate Express starter app
-const express = require('express')
+const express = require('express');
+const bodyParser = require('body-parser');
 const app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs')
 
+app.get('/', function (req, res) {
+  res.render('index');
+})
+app.post('/', function (req, res) {
+  console.log(req.body.city);
+  res.render('index');
+})
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
