b0051717637b80e9e5627024eced405a1c90832c
helenpidoulas <helen.pidoulas@gmail.com>
2019-03-19 23:05:17 +1100

    progress commit

--- weather-app-nodejs/weather-app-nodejs/routes/index.js
+++ weather-app-nodejs/weather-app-nodejs/routes/index.js
@@ -3,7 +3,29 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+ res.render('index', {'body':'', forecast: ''});
 });
 
 module.exports = router;
+
+let url    = 'http://api.openweathermap.org/data/2.5/weather?q='
+let appId  = '8a1a3cc956c204ebbbd40834bdd9dc38';
+
+let units  = '&units=metric';
+
+
+var request = require('request');
+
+router.post('/weather', function(req, res, next){
+  let city = req.body.city;
+  url = url+city+"&"+appId;
+ request(url, function (error, response, body) {
+      body = JSON.parse(body);
+      if(error && response.statusCode != 200){
+        throw error;
+      }
+    let country = (body.sys.country) ? body.sys.country : '' ;
+    let forecast = "For city "+city+', country '+country;
+    res.render('index', {body : body, forecast: forecast});
+   });
+});
